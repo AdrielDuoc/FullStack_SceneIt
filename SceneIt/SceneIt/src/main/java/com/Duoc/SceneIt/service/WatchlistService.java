57b51d3e7df9a921bcf3d162c0906e751ce5bad1
adrielDuoc <adr.yanez@duocuc.cl>
2026-04-23 12:36:18 -0400

    Se creo Repository y service para Watchlist

---
+++ SceneIt/SceneIt/src/main/java/com/Duoc/SceneIt/service/WatchlistService.java
@@ -0,0 +1,37 @@
+package com.Duoc.SceneIt.service;
+
+import com.Duoc.SceneIt.modelo.Watchlist;
+import com.Duoc.SceneIt.repository.WatchlistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WatchlistService {
+
+    @Autowired
+    private WatchlistRepository watchlistRepository;
+
+    public Watchlist getWatchlist () {
+        return (Watchlist) watchlistRepository.findAll();
+    }
+
+    public Watchlist saveWatchlist(Watchlist watchlist){
+        return watchlistRepository.save(watchlist);
+    }
+
+    public Watchlist getWatchlistId(Integer id){
+        return watchlistRepository.findById(id).orElse(null);
+    }
+
+    public void deleteWatchlist(Integer id){
+        watchlistRepository.deleteById(id);
+    }
+
+    public Watchlist updateWatchlist(Watchlist watchlist){
+        if(!watchlistRepository.existsById(watchlist.getId_watchlist())){
+            return null;
+        }
+        return watchlistRepository.save(watchlist);
+    }
+
+}
