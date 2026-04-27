8096909bf9302a77ba0fb0e42f4877f00789b7cb
Javier <ja.fuentealbaz@duocuc.cl>
2026-04-27 09:40:01 -0400

    Arreglo de atributo edad_autor del modelo Autor int a Integer

--- SceneIt/SceneIt/src/main/java/com/Duoc/SceneIt/modelo/Autor.java
+++ SceneIt/SceneIt/src/main/java/com/Duoc/SceneIt/modelo/Autor.java
@@ -1,6 +1,10 @@
 package com.Duoc.SceneIt.modelo;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -23,7 +27,7 @@ public class Autor {
     private String nombre_autor;
 
     @NotNull
-    private int edad_autor;
+    private Integer edad_autor;
 }
 
 
