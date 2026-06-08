c389bcd832028822a296b4a09ddcc4c0b71dedb1
javier <ja.fuentealbaz@duocuc.cl>
2026-06-08 09:40:54 -0400

    Creacion de archivos ControllerTest

---
+++ SceneIt/SceneIt/src/test/java/com/Duoc/SceneIt/controllerTest/AutorControllerTest.java
@@ -0,0 +1,12 @@
+package com.Duoc.SceneIt.controllerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AutorControllerTest {
+
+}
---
+++ SceneIt/SceneIt/src/test/java/com/Duoc/SceneIt/controllerTest/PeliculaControllerTest.java
@@ -0,0 +1,14 @@
+package com.Duoc.SceneIt.controllerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PeliculaControllerTest {
+
+    
+
+}
